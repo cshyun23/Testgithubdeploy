7fcff41157cb58fa49a646fd72f1e34cf41e8b45
cshyun23 <cshyun23@naver.com>
2019-09-04 10:02:24 +0900

    test

---
+++ src/main/java/Deploytest/Forgithub/App.java
@@ -0,0 +1,13 @@
+package Deploytest.Forgithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    void TestApp() {
+    	
+    	System.out.println("Github dependency test success!!");
+    }
+}
